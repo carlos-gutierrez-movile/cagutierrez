a22a5fee6e1117fb02c76036b01e96f74cf2c6cb
Carlos Andres Gutierrez Castro <carlos.gutierrez@movile.com>
2013-01-31 13:51:41 -0500

    Prueba de GIT en Eclipse

---
+++ Prueba/src/inicial.java
@@ -0,0 +1,12 @@
+
+public class inicial {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prueba de correo");
+	}
+
+}
