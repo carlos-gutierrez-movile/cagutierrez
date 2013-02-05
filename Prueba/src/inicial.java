3a5cde78fbba1dd9537ab04c42f62638a75264cf
cagutierrez <carlos.gutierrez@movile.com>
2013-02-05 13:47:46 -0500

    Modified

--- Prueba/src/inicial.java
+++ Prueba/src/inicial.java
@@ -6,7 +6,7 @@ public class inicial {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Prueba de correo");
+		System.out.println("Prueba de correo  modificado");
 		System.out.println("Gracias");			
 	}
 
